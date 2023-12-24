46b523e020440d3a859132496f81126e0b6d706c
David <dbarron410@vt.edu>
2023-12-24 17:20:51 -0500

    repo update

--- popup/popup.js
+++ popup/popup.js
@@ -1,10 +1,79 @@
 
+document.getElementById('toggleHubButton').addEventListener('click', function() {
+    var hubContainer = document.querySelector('.hub-container');
+    var extensionInteraction = document.querySelector('.extension-interaction');
+    var toggleHubButton = document.getElementById('toggleHubButton'); 
 
-document.getElementById('toggleGuideButton').addEventListener('click', function() {
-    var guide = document.getElementById('how-to');
-    if (guide.style.display === 'none') {
-        guide.style.display = 'block';
+    if (hubContainer.style.display === 'none' || !hubContainer.style.display) {
+        hubContainer.style.display = 'block';
+        extensionInteraction.style.display = 'block';
+        toggleHubButton.style.display = 'none'; 
+
+        hubContainer.style.marginTop = '60px'; 
+        hubContainer.scrollIntoView({ behavior: 'smooth' });
     } else {
-        guide.style.display = 'none';
+        hubContainer.style.display = 'none';
+        extensionInteraction.style.display = 'none';
+        toggleHubButton.style.display = 'block'; 
+
+        document.body.scrollIntoView({ behavior: 'smooth' });
     }
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.hub-container').style.display = 'none'; 
+
+    var searchInput = document.getElementById('searchInput');
+    var dropdownContent = document.getElementById('dropdownContent');
+    var items = ['Weather Retrieval', 'Music Control', 'Web Browser', 'News Rerieval']; 
+
+    function showItems() {
+        dropdownContent.innerHTML = ''; 
+        items.forEach(function(item) {
+            var div = document.createElement('div');
+            div.textContent = item;
+            div.onclick = function() { selectItem(item); };
+            dropdownContent.appendChild(div);
+        });
+        dropdownContent.style.display = 'block';
+    }
+
+    function selectItem(item) {
+        console.log('Selected:', item);
+        dropdownContent.style.display = 'none';
+    }
+
+    searchInput.addEventListener('click', showItems);
+
+    document.addEventListener('click', function(event) {
+        if (!searchInput.contains(event.target)) {
+            dropdownContent.style.display = 'none';
+        }
+    });
+});
+
+let selectedValues = ['Basic', 'Personalization']; 
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    updateSelectedItemsDisplay();
+});
+
+function updateSelectedItemsDisplay() {
+    const container = document.querySelector('.selected-items');
+    container.innerHTML = ''; 
+    selectedValues.forEach((value) => {
+        const item = document.createElement('div');
+        item.className = 'selected-item';
+        item.textContent = value;
+        const removeBtn = document.createElement('span');
+        removeBtn.textContent = 'x';
+        removeBtn.onclick = () => {
+            selectedValues = selectedValues.filter((val) => val !== value);
+            updateSelectedItemsDisplay(); 
+        };
+        item.appendChild(removeBtn);
+        container.appendChild(item);
+    });
+}
+
