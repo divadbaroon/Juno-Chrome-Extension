ad2b271c026e38ec2570664f49f6097e244b11e1
David <dbarron410@vt.edu>
2024-09-05 23:08:30 -0700

    file name change

--- src/background.ts
+++ src/background.ts
@@ -2,5 +2,5 @@
 Background listener is responsible for listening for messages from content script
 and popup. Specifcally for initialzing the user's data, and interacting with the selected LLM.
 */
-import './listeners/listener'
+import './fileCommunication/listeners'
 
