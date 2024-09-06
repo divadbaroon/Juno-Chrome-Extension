1711fef050a890fe9a00ba1da2d0abb075c08e49
David <dbarron410@vt.edu>
2024-09-05 23:08:59 -0700

    temporarily adding function for handling GUI response to user

--- src/content.ts
+++ src/content.ts
@@ -6,3 +6,6 @@ which includes the following functions:
 Speech recognition -> intent recognition -> call extension (if applicable) -> LLM interaction -> Text-to-Speech
 */
 import "./orchestrator/orchestrator"
+
+import "./functions/guiResponse/handleGUIResponse"
+
